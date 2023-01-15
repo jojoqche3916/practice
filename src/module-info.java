a18937dcb511dd649be9477ccd8dac98e4d8086b
Jodie <yourgitemail@example.com>
2023-01-16 01:27:18 +1100

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class  {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("hello world!");
+	}
+}
---
+++ src/practice/learning.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class learning {
+
+	public static void main(String[] args) {
+		addExclamationPoint("HOT DOGS");P
+		
+	}
+    public static void addExclamationPoint(String s) {
+    	System.out.println(s+"!");
+    }
+}
